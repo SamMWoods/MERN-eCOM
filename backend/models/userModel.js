fe3b138ab9379fb671d2fed49112ac99a86ac7b7
Sam Woods <sammichaelwoods@gmail.com>
2021-06-22 17:04:31 +0100

    Back-end Authentication

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,28 +1,45 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
-const userScheme = mongoose.Schema({
-    name:{
-        type: String,
-        require: true
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    email:{
-        type: String,
-        require: true,
-        unique:true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type: String,
-        require: true
+    password: {
+      type: String,
+      required: true,
     },
-    isAdmin:{
-        type: Boolean,
-        require: true,
-        default: false
-    }
-},{
-    timestamps: true
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password)
+}
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next()
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
 })
 
-const User = mongoose.model('User', userScheme)
+const User = mongoose.model('User', userSchema)
 
 export default User
--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,21 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import connectBD from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 connectBD()
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('API is running')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 //error middleware
 app.use(notFound)
